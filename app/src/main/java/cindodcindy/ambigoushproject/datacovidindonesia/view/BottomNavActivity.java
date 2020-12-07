d80e1045d9769afe8215dd95b9f20021283ed22c
CindoddCindy <cindodcindy@gmail.com>
2020-12-07 13:58:06 +0700

    layout hehe

--- app/src/main/java/cindodcindy/ambigoushproject/datacovidindonesia/view/BottomNavActivity.java
+++ app/src/main/java/cindodcindy/ambigoushproject/datacovidindonesia/view/BottomNavActivity.java
@@ -1,10 +1,17 @@
 package cindodcindy.ambigoushproject.datacovidindonesia.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import cindodcindy.ambigoushproject.datacovidindonesia.R;
+import cindodcindy.ambigoushproject.datacovidindonesia.view.fragment.FragmentCovid;
+import cindodcindy.ambigoushproject.datacovidindonesia.view.fragment.FragmentNews;
 
 public class BottomNavActivity extends AppCompatActivity {
 
@@ -12,5 +19,44 @@ public class BottomNavActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_nav);
+
+
+        getFragmentPage(new FragmentCovid());
+
+        BottomNavigationView bottomNavigation = findViewById(R.id.bottomNavigationView);
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                Fragment fragment = null;
+
+                //Menantukan halaman Fragment yang akan tampil
+                switch (item.getItemId()){
+                    case R.id.fr_satu:
+                        fragment = new FragmentCovid();
+                        break;
+
+                    case R.id.fr_dua:
+                        fragment = new FragmentNews();
+                        break;
+
+
+                }
+                return getFragmentPage(fragment);
+            }
+        });
+
+    }
+
+    //Menampilkan halaman Fragment
+    private boolean getFragmentPage(Fragment fragment){
+        if (fragment != null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.page_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 }
--- app/src/main/java/cindodcindy/ambigoushproject/datacovidindonesia/view/fragment/FragmentCovid.java
+++ app/src/main/java/cindodcindy/ambigoushproject/datacovidindonesia/view/fragment/FragmentCovid.java
@@ -3,10 +3,19 @@ package cindodcindy.ambigoushproject.datacovidindonesia.view.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import cindodcindy.ambigoushproject.datacovidindonesia.R;
 
@@ -25,6 +34,12 @@ public class FragmentCovid extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+
+    private View view;
+    private Adapter adapter;
+    private Bundle savedInstanceState;
+
 
     public FragmentCovid() {
         // Required empty public constructor
@@ -61,6 +76,57 @@ public class FragmentCovid extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_covid, container, false);
+        View view = inflater.inflate(R.layout.fragment_covid, container, false);
+
+        ViewPager viewPager = (ViewPager) view.findViewById(R.id.view_pager);
+        setupViewPager(viewPager);
+        // Set Tabs inside Toolbar
+        TabLayout tabs = (TabLayout) view.findViewById(R.id.tab_layout);
+        tabs.setupWithViewPager(viewPager);
+
+
+        return view;
     }
+
+    private void setupViewPager(ViewPager viewPager) {
+
+
+        Adapter adapter = new Adapter(getChildFragmentManager());
+        adapter.addFragment(new DataIndonesiaFragmnet(), "Indonesia");
+        adapter.addFragment(new DataGlobal(), "Global");
+        viewPager.setAdapter(adapter);
+
+
+
+    }
+
+    static class Adapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public Adapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
 }
