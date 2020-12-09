3e3ae2856383a940ae6b35ff75c66fc500d5b5ec
CindoddCindy <cindodcindy@gmail.com>
2020-12-09 08:44:54 +0700

    tambah news

--- app/src/main/java/cindodcindy/ambigoushproject/datacovidindonesia/view/adapter/SliderAdapterExample.java
+++ app/src/main/java/cindodcindy/ambigoushproject/datacovidindonesia/view/adapter/SliderAdapterExample.java
@@ -48,61 +48,40 @@ public class SliderAdapterExample extends SliderViewAdapter<SliderAdapterExample
 
         switch (position){
             case 0:
-                viewHolder.imageViewBackground_drink.setImageResource(news);
+                viewHolder.imageViewBackground_drink.setImageResource(news_bekrond);
                 break;
 
             case 1:
-                Glide.with(viewHolder.itemView)
-                        .load("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRpUGwWMpAh1-NtsrOuKHYXZs7V9eoSZx7SLCib8m__nnZcMkU9&usqp=CAU")
-                        .into(viewHolder.imageViewBackground_drink);
+                viewHolder.imageViewBackground_drink.setImageResource(news);
                 break;
 
             case 2:
-                Glide.with(viewHolder.itemView)
-                        .load("https://dailyburn.com/life/wp-content/uploads/2015/03/The-New-Juicing-2.jpg")
-                        .into(viewHolder.imageViewBackground_drink);
+                viewHolder.imageViewBackground_drink.setImageResource(slider_news_dua);
                 break;
 
             case 3:
-                Glide.with(viewHolder.itemView)
-                        .load("https://media.mnn.com/assets/images/2017/05/GlassOrangeJuiceHalvedOrangesBlueBackground.jpg.838x0_q80.jpg")
-                        .into(viewHolder.imageViewBackground_drink);
+                viewHolder.imageViewBackground_drink.setImageResource(slider_news_tiga);
                 break;
 
             case 4:
-                Glide.with(viewHolder.itemView)
-                        .load("https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2016/05/milkshake.jpg?itok=n_3au7e1")
-                        .into(viewHolder.imageViewBackground_drink);
+                viewHolder.imageViewBackground_drink.setImageResource(slider_news_empat);
                 break;
             case 5:
-                Glide.with(viewHolder.itemView)
-                        .load("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT4rdES0fOAmvJTpCY8r9gxGcntWVUr36hxCOkvRL4NqnrcHqpl&usqp=CAU")
-                        .into(viewHolder.imageViewBackground_drink);
+                viewHolder.imageViewBackground_drink.setImageResource(slider_news_lima);
                 break;
             case 6:
-                Glide.with(viewHolder.itemView)
-                        .load("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQEmeyjNeXI-95VPeQHYP36bGpx7KMCtZqWdQuI0gOVbwvf_RNO&usqp=CAU")
-                        .into(viewHolder.imageViewBackground_drink);
+                viewHolder.imageViewBackground_drink.setImageResource(slider_news_enam);
                 break;
             case 7:
-                Glide.with(viewHolder.itemView)
-                        .load("https://cdn1-production-images-kly.akamaized.net/AQXr7OxPNGsBxFzKXvx43U85KFY=/640x360/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/2846605/original/095930000_1562474255-iStock-544468096.jpg")
-                        .into(viewHolder.imageViewBackground_drink);
+                viewHolder.imageViewBackground_drink.setImageResource(slider_new_tujuh);
                 break;
 
             case 8:
-                Glide.with(viewHolder.itemView)
-                        .load("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSSBtGcj29TaCsNXmIMg61Lg_Cc_9ExPWarqjfClKH9vqwVBsHw&usqp=CAU")
-                        .into(viewHolder.imageViewBackground_drink);
+                viewHolder.imageViewBackground_drink.setImageResource(news_sembilan);
                 break;
 
-
-
-
             default:
-                Glide.with(viewHolder.itemView)
-                        .load("https://upload.wikimedia.org/wikipedia/commons/b/b4/Penyajian_Infused_Water.jpg")
-                        .into(viewHolder.imageViewBackground_drink);
+                viewHolder.imageViewBackground_drink.setImageResource(slider_news);
                 break;
 
         }
