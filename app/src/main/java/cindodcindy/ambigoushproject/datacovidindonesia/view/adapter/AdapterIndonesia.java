785cfef42a969ea3da0c5d68e22d2acc01173320
CindoddCindy <cindodcindy@gmail.com>
2020-12-09 16:07:08 +0700

    rv masih salah

--- app/src/main/java/cindodcindy/ambigoushproject/datacovidindonesia/view/adapter/AdapterIndonesia.java
+++ app/src/main/java/cindodcindy/ambigoushproject/datacovidindonesia/view/adapter/AdapterIndonesia.java
@@ -1,33 +1,64 @@
 package cindodcindy.ambigoushproject.datacovidindonesia.view.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
+import cindodcindy.ambigoushproject.datacovidindonesia.R;
+import cindodcindy.ambigoushproject.datacovidindonesia.model.PropinsiAtributes;
+
 public class AdapterIndonesia  extends RecyclerView.Adapter<AdapterIndonesia.IndonesiaAdapterChild> {
+
+
+    Context context;
+    List<PropinsiAtributes> propinsiAtributes;
+
+    public AdapterIndonesia(Context context, List<PropinsiAtributes> propinsiAtributesList){
+        this.context=context;
+        this.propinsiAtributes=propinsiAtributesList;
+    }
 
     @NonNull
     @Override
     public IndonesiaAdapterChild onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
-    }
+
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View views= layoutInflater.inflate(R.layout.item_propinsi, parent, false);
+        IndonesiaAdapterChild adapterChild=new IndonesiaAdapterChild(views);
+
+        return adapterChild;
+       }
 
     @Override
     public void onBindViewHolder(@NonNull IndonesiaAdapterChild holder, int position) {
+        holder.textView_nama_prop.setText(propinsiAtributes.get(position).getProvinsi());
+//        holder.textView_isi.setText(modelSementaras.get(position).isi);
+
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return propinsiAtributes.size();
     }
 
     public  class IndonesiaAdapterChild extends RecyclerView.ViewHolder{
 
+        private TextView textView_nama_prop, textView_positif, textView_meninggal, textView_sembuh;
+
         public IndonesiaAdapterChild(@NonNull View itemView) {
             super(itemView);
+            textView_nama_prop=itemView.findViewById(R.id.tv_prop_nama_propinsi);
+            textView_positif=itemView.findViewById(R.id.tv_propinsi_positif);
+            textView_meninggal=itemView.findViewById(R.id.tv_propinsi_meninggal);
+            textView_sembuh=itemView.findViewById(R.id.tv_propinsi_sembuh);
         }
     }
 }
--- app/src/main/java/cindodcindy/ambigoushproject/datacovidindonesia/view/fragment/DataIndonesiaFragmnet.java
+++ app/src/main/java/cindodcindy/ambigoushproject/datacovidindonesia/view/fragment/DataIndonesiaFragmnet.java
@@ -12,18 +12,27 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
 import com.smarteist.autoimageslider.SliderAnimations;
 import com.smarteist.autoimageslider.SliderView;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import cindodcindy.ambigoushproject.datacovidindonesia.R;
 import cindodcindy.ambigoushproject.datacovidindonesia.model.ModelTest;
+import cindodcindy.ambigoushproject.datacovidindonesia.model.PropinsiAtributes;
+import cindodcindy.ambigoushproject.datacovidindonesia.model.PropinsiResponse;
 import cindodcindy.ambigoushproject.datacovidindonesia.retrofit.RetrofitMethod;
+import cindodcindy.ambigoushproject.datacovidindonesia.retrofit.RetrofitUrl;
+import cindodcindy.ambigoushproject.datacovidindonesia.view.adapter.AdapterIndonesia;
 import cindodcindy.ambigoushproject.datacovidindonesia.view.adapter.SliderAdapterIndo;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -40,6 +49,11 @@ public class DataIndonesiaFragmnet extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private RecyclerView recyclerView;
+    private AdapterIndonesia adapterIndonesia;
+    private List<PropinsiAtributes> propinsiAtributes = new ArrayList<>();
+    private RetrofitMethod retrofitMethod;
 
 
 
@@ -98,6 +112,70 @@ public class DataIndonesiaFragmnet extends Fragment {
 
         return view;
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        recyclerView = view.findViewById(R.id.rv_indo_data_prop);
+        adapterIndonesia = new AdapterIndonesia( getContext(),propinsiAtributes);
+        recyclerView.setAdapter(adapterIndonesia);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+        getDataPropinsi();
+
+
+
+    }
+
+
+    public void getDataPropinsi(){
+
+        retrofitMethod = RetrofitUrl.getRetrofitHandleDataCovid().create(RetrofitMethod.class);
+        Call<PropinsiResponse> orderListCall=retrofitMethod.getDataPropinsi();
+        orderListCall.enqueue(new Callback<PropinsiResponse>() {
+            @Override
+            public void onResponse(Call<PropinsiResponse> call, Response<PropinsiResponse> response) {
+
+                if (response.isSuccessful()) {
+                    List<PropinsiAtributes> propinsiAtributes= (List<PropinsiAtributes>) response.body();
+                    adapterIndonesia = new AdapterIndonesia(getContext(),propinsiAtributes);
+                    recyclerView.setAdapter(adapterIndonesia);
+                    adapterIndonesia.notifyDataSetChanged();
+                }
+                else {
+                    // error case
+                    switch (response.code()) {
+                        case 404:
+                            Toast.makeText(getContext(), "404 not found", Toast.LENGTH_SHORT).show();
+                            break;
+                        case 500:
+                            Toast.makeText(getContext(), "500 internal server error", Toast.LENGTH_SHORT).show();
+                            break;
+                        case 401:
+                            Toast.makeText(getContext(), "401 unauthorized", Toast.LENGTH_SHORT).show();
+                            break;
+
+                        default:
+                            Toast.makeText(getContext(), "unknown error", Toast.LENGTH_SHORT).show();
+                            break;
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<PropinsiResponse> call, Throwable t) {
+                Toast.makeText(getContext(), "network failure :( inform the user and possibly retry ", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+    }
+
+
+
+
 
     /*
 
